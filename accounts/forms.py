c2f7a9a0741aba78059a949214363d68d87bdf5b
Rhy <yhr@xs4all.nl>
2020-01-11 14:35:43 +0000

    added start of logout functionality

---
+++ accounts/forms.py
@@ -0,0 +1,8 @@
+"""test file from xav class"""
+
+from django import forms
+from .models import Movies
+class MoviesForm(forms.ModelForm):
+    class Meta:
+        model = Movies
+        fields = ('name', 'description', 'time', 'authors', 'realisator')
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth
 
 # Create your views here.
+
+def index(request):
+    """Return the index.html file"""
+    return render(request, 'index.html')
+
+def create_movie(request):
+    return render(request, 'movie-create.html')
+
+def logout(request):
+    """ log the user out """
+    auth.logout(request)
+    return redirect(reverse('index'))
