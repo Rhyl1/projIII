d940d1bd94430e92cccf13a471dbdb4cfa0f6872
Rhy <yhr@xs4all.nl>
2020-01-12 15:19:24 +0000

    added extra functionality to login page as in course content

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,14 @@
 """test file from xav class"""
 
 from django import forms
-from .models import Movies
-class MoviesForm(forms.ModelForm):
-    class Meta:
-        model = Movies
-        fields = ('name', 'description', 'time', 'authors', 'realisator')
+# from .models import Movies
+# class MoviesForm(forms.ModelForm):
+#     class Meta:
+#         model = Movies
+#         fields = ('name', 'description', 'time', 'authors', 'realisator')
+
+
+class UserLoginForm(forms.Form):
+    """ Form to be used to log users in"""
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
