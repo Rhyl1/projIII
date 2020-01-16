efc8ec4e6bce544840d7656230f077d297bab912
Rhy <yhr@xs4all.nl>
2020-01-16 11:36:28 +0000

    added login functionality as stated in course material

--- accounts/views.py
+++ accounts/views.py
@@ -13,10 +13,22 @@ def create_movie(request):
 def logout(request):
     """ log the user out """
     auth.logout(request)
-    messages.success(request, "You have succesfully been logged out")
+    messages.success(request, "You have succesfully been logged out!")
     return redirect(reverse('index'))
 
 def login(request):
     """Return a login page"""
-    login_form = UserLoginForm()
-    return render(request, 'login.html', {"login_form":login_form})
+    if request.method == "POST":
+        login_form = UserLoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+            
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!")
+            else:
+                login_form.add_error(None, "Your username or password is incorrect")
+    else:
+        login_form = UserLoginForm()
+    return render(request, 'login.html', {'login_form': login_form})
